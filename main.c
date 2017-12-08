96c95a0f678fe34f7352a696ffa7023f263581eb
fabbbbbiola <fradosav@stuy.edu>
2017-12-08 10:53:24 -0500

    stuff

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-
+  char input[256];
+  printf("Input:\t");
+  fgets(input, sizeof(input), stdin);
+  printf("%s\n", input);
+  
+  int i = scanf(input);
+  //  shmat( , &, );
 }
